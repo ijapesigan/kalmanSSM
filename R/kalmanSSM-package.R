1be963eebb47b8a80c2898cfcc7d053c556792fc
ijapesigan <ijapesigan@users.noreply.github.com>
2023-09-04 16:30:08 +0000

    Update src to output generated at 2cadb06

---
+++ R/kalmanSSM-package.R
@@ -0,0 +1,9 @@
+#' @aliases kalmanSSM-package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib kalmanSSM, .registration = TRUE
+## usethis namespace: end
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(kalmanSSM)
+
+test_check("kalmanSSM")
