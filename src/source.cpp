cae2cf03c026521c200c9f83b294ef4870b23dbc
ijapesigan <ijapesigan@users.noreply.github.com>
2023-09-06 02:55:02 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/kalmanSSM.pdf
    A	.setup/build/kalmanSSM_0.0.0.9000.tar.gz
    M	.setup/cpp/kalmanSSM-k-filter-p-1.cpp
    A	.setup/latex/bib/quarto.bib
    A	.setup/lint/.lintr
    A	.setup/quarto/bib/quarto.bib
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE.md
    A	NAMESPACE
    A	R/RcppExports.R
    A	README.md
    A	man/KFilterP1.Rd
    A	man/dat_multiv_p1.Rd
    A	man/dat_univ_p1.Rd
    A	man/kalmanSSM-package.Rd
    M	project.Rproj
    A	src/Makevars
    A	src/Makevars.win
    A	src/RcppExports.cpp
    A	src/source.cpp
    M	vignettes/dat_univ_p1.Rmd
    A	vignettes/vignettes.bib

--- src/source.cpp
+++ src/source.cpp
@@ -1,4 +1,16 @@
 // -----------------------------------------------------------------------------
+// edit .setup/cpp/000-forward-declarations.cpp
+// Ivan Jacob Agaloos Pesigan
+// -----------------------------------------------------------------------------
+
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+Rcpp::List KFilterLag1(const arma::mat& data, const arma::mat& Lambda,
+                       const arma::mat& mu0, const arma::mat& Sigma0,
+                       const arma::mat& beta, const arma::mat& chol_psi,
+                       const arma::mat& chol_theta);
+// -----------------------------------------------------------------------------
 // edit .setup/cpp/kalmanSSM-k-filter-lag-1.cpp
 // Ivan Jacob Agaloos Pesigan
 // -----------------------------------------------------------------------------
